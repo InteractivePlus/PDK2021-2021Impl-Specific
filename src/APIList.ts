cc9df2a55677448f2e4e079d31eb8314320d06ad
Windy Feng <1745500559@qq.com>
2021-08-10 06:37:34 +0800

    Add

---
+++ src/APIList.ts
@@ -0,0 +1,7 @@
+import { PDKAPI } from '@interactiveplus/pdk2021-common/dist/ExchangeDataTypes/PDKAPI'
+
+const allPDKAPIList : PDKAPI<any,any,any>[] = [];
+
+const 
+
+export default allPDKAPIList;
